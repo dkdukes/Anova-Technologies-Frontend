7f8058a4ddb6df92addd948cf81a4b7fb006b7e5
dkdukes <fedulike@gmail.com>
2026-09-09 13:06:54 +0300

    feat: modify the api file to allow for login using access tokens

--- src/api.js
+++ src/api.js
@@ -1,7 +1,25 @@
+
 import axios from "axios";
 
 const api = axios.create({
     baseURL: "http://127.0.0.1:8000/api/",
 });
 
+// Attach JWT access token to authenticated requests
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("accessToken");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 export default api;
+
